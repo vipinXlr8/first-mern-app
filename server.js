8c079a195dab3fc992a12921e518e8d0a39f9f4c
vipinXlr8 <vipinm922@gmail.com>
2022-12-04 00:18:08 +0530

    routes-commit

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect DB
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=> res.send('API Running'));
+
+// Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
